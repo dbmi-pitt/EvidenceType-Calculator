ee6763210cf0268546f34c062a833a3f27dc00d8
Yifan Ning <ningyifan1212@163.com>
2017-08-22 15:37:58 -0400

    add controller for ddi clinical trial and case report

--- source/evidence_type_calculator/controllers/forms.py
+++ source/evidence_type_calculator/controllers/forms.py
@@ -4,3 +4,17 @@ def index():
 
     response.flash = T("test")
     return dict(message=T('evidence type form!'))
+
+
+def clinicalTrialForm():
+
+    print '[INFO] controller forms: clinicalTrialForm()...'
+    print request.vars
+    return dict(message=T('clinical trial submit!'))
+
+
+def caseReportForm():
+
+    print '[INFO] controller forms: caseReportForm()...'
+    print request.vars
+    return dict(message=T('clinical trial submit!'))
