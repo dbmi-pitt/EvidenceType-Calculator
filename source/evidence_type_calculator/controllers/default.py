287516a838b969c6fd21997770fd9cc85ad2cef8
Yifan Ning <ningyifan1212@163.com>
2017-08-30 10:39:27 -0400

    bug fix on task summary panel

--- source/evidence_type_calculator/controllers/default.py
+++ source/evidence_type_calculator/controllers/default.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-# this file is released under public domain and you can use without limitations
-
 def index():
     print '[INFO] default controller index...'
 
@@ -16,34 +14,39 @@ def index():
     return dict(login_form = form, task_summary_table = "")
 
 
+# update task summary table based on participant selection
 def taskCallback():
     print '[INFO] default task callback...'
     print request.vars
 
-    part_name = request.vars.participant
-    tasks = getTasksByParticipant(part_name)
-    # print tasks
+    tasks = getTasksByParticipant(request.vars.participant)
 
-    table = "<table class='table'><tr><td>participant</td><td>task</td><td>with assist? (T=Yes, F=No)</td><td>link</td></tr>"
-    for task in tasks:
-        table += "<tr><td>%s</td><td>%s</td><td>%s</td><td><a href='%s'>%s<a></td></tr>" % (task[0], task[1], task[2], URL('redirectToForm'), task[1])
-    table += "</table>"
+    table = "<table class='table'><tr><td>participant</td><td>task</td><td>with assist? (T=Yes, F=No)</td><td>is started</td><td>is finished</td><td>link</td></tr>"
+    rows= ""
+    
+    # get summary table contents if valid participant is selected
+    if request.vars.participant and request.vars.participant != "select":
+        for task in tasks:
+            rows += "<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td><a href='%s'>%s<a></td></tr>" % (task[0], task[1], task[2], task[3], task[4], URL('redirectToForm'), "task " + str(task[1]))
+        
+    table += rows + "</table>"
 
     r = 'jQuery("#summary_table").html("%s")' % table
-    # print r
     return r
 
 
-# jump to form
+# jump to evidence question form
 def redirectToForm():
     redirect(URL(request.application, 'forms', 'index'))
 
     
 # get all tasks assigned to the participant
+# return list of tasks (participant, task_id, is assist, is started, is_finished)
 def getTasksByParticipant(participant):
-    sql = "SELECT participant, task_id, assist FROM participant_task WHERE participant='%s';" % participant
-    tasks = db.executesql(sql)
-    return tasks
+    # sql = "SELECT participant, task_id, assist FROM participant_task WHERE participant='%s';" % participant
+    sql = "SELECT p.participant, p.task_id, p.assist, e.is_started, e.is_finished FROM participant_task p LEFT JOIN evidence_type e ON p.participant=e.participant AND p.task_id = e.task_id WHERE p.participant='%s';" % (participant)
+    print "[DEBUG] get tasks by participant: " + sql
+    return db.executesql(sql)
 
 
 # get list of participants
