89e11d44da556bad444d0ef3dd5c996f5f9116c8
Yifan Ning <ningyifan1212@163.com>
2017-08-31 15:19:59 -0400

    revised activity diagram and ER diagram

--- source/evidence_type_calculator/models/index.py
+++ source/evidence_type_calculator/models/index.py
@@ -1,7 +1,7 @@
 import os, sys
 
 ## maps for question code and full question content
-qsMap = {"ct-gr": "Group Randomization?", "ct-pgd": "Parallel Group Design?", "ct-pk": "Study Focused on Pharmacokinetic Processes?", "ct-ph": "Phenotyping?", "ct-gt": "Genotyping?", "cr-ae": "Reporting an adverse event?", "cr-pr": "Publically reported?", "cr-ep": "Following an evaluation protocol?", "exp-m-st": "Subtype?", "exp-m-at": "Assay Type?", "exp-m-mi": "Metabolic Inhibitor?", "exp-t-st": "Subtype?", "exp-t-at": "Assay Type?", "exp-t-tp": "Transporter Protein?"}
+qsMap = {"ct-gr": "Group Randomization?", "ct-pgd": "Parallel Group Design?", "ct-pk": "Study Focused on Pharmacokinetic Processes?", "ct-ph": "Phenotyping?", "ct-gt": "Genotyping?", "cr-ae": "Reporting an adverse event?", "cr-pr": "Publically reported?", "cr-ep": "Icl an evaluation protocol?", "exp-m-st": "Subtype?", "exp-m-at": "Assay Type?", "exp-m-mi": "Metabolic Inhibitor?", "exp-t-st": "Subtype?", "exp-t-at": "Assay Type?", "exp-t-tp": "Transporter Protein?"}
 
 db = DAL('sqlite://storage.sqlite', fake_migrate=True)
 
@@ -10,19 +10,19 @@ db.define_table('evidence_type_form', Field('is_started', type='boolean'), Field
 
 db.define_table('evidence_type_question', Field('evidence_type_form_id', db.evidence_type_form), Field('question', type='string'), Field('answer', type='string'))
 
-# following form
-db.define_table('following_form', Field('is_started', type='boolean'), Field('is_finished', type='boolean'))
+# icl form
+db.define_table('icl_form', Field('is_started', type='boolean'), Field('is_finished', type='boolean'))
 
-db.define_table('following_question', Field('following_form_id', db.following_form), Field('question', type='string'), Field('answer', type='string'))
+db.define_table('icl_question', Field('icl_form_id', db.icl_form), Field('question', type='string'), Field('answer', type='string'))
 
 # sub method type
 db.define_table('sub_method', Field('sub_method_name', type='string'), Field('sub_method_level', type='integer'))
 
 # evidence type
-db.define_table('evidence_type', Field('task_id', type='string'), Field('participant', type='string'), Field('mp_method', type='string'), Field('evidence_type_form_id', db.evidence_type_form), Field('following_form_id', db.following_form), Field('sub_method_id', db.sub_method), Field('inferred_evidence_type', type='string'), Field('is_started', type='boolean'), Field('is_finished', type='boolean'), Field('is_agree_with_inference', type='boolean'), Field('disagree_comment', type='string'))
+db.define_table('evidence_type', Field('task_id', type='string'), Field('participant', type='string'), Field('mp_method', type='string'), Field('evidence_type_form_id', db.evidence_type_form), Field('icl_form_id', db.icl_form), Field('sub_method_id', db.sub_method), Field('inferred_evidence_type', type='string'), Field('entered_evidence_type', type='string'), Field('is_started', type='boolean'), Field('is_finished', type='boolean'), Field('is_agree_with_inference', type='boolean'), Field('disagree_comment', type='string'))
 
 # participant task
-db.define_table('participant_task', Field('participant', type='string'), Field('task_id', type='integer'), Field('assist', type='boolean'))
+db.define_table('participant_task', Field('participant', type='string'), Field('code', type='string'), Field('task_id', type='integer'), Field('assist', type='boolean'))
 
 ## clean all tables
 db.evidence_type.truncate()
