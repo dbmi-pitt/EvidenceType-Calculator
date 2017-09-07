dbedf7b077b6bcf6700c1ced8a1b3e7d34ba5636
Yifan Ning <ningyifan1212@163.com>
2017-09-07 10:11:43 -0400

    add meet ic and confidence to DB

--- source/evidence_type_calculator/models/summary.py
+++ source/evidence_type_calculator/models/summary.py
@@ -22,7 +22,7 @@ db.define_table('icl_question', Field('icl_form_id', db.icl_form), Field('questi
 db.define_table('sub_method', Field('sub_method_name', type='string'), Field('sub_method_level', type='integer'))
 
 # evidence type
-db.define_table('evidence_type', Field('task_id', type='string'), Field('participant_code', type='string'), Field('mp_method', type='string'), Field('evidence_type_form_id', db.evidence_type_form), Field('icl_form_id', db.icl_form), Field('sub_method_id', db.sub_method), Field('inferred_evidence_type', type='string'), Field('entered_evidence_type', type='string'), Field('is_started', type='boolean'), Field('is_finished', type='boolean'), Field('is_agree_with_inference', type='boolean'), Field('disagree_comment', type='string'))
+db.define_table('evidence_type', Field('task_id', type='string'), Field('participant_code', type='string'), Field('mp_method', type='string'), Field('evidence_type_form_id', db.evidence_type_form), Field('icl_form_id', db.icl_form), Field('sub_method_id', db.sub_method), Field('inferred_evidence_type', type='string'), Field('entered_evidence_type', type='string'), Field('is_started', type='boolean'), Field('is_finished', type='boolean'), Field('is_agree_with_inference', type='boolean'), Field('is_meet_inclusion_criteria', type='boolean'), Field('confidence', type='string'), Field('disagree_comment', type='string'))
 
 # participant task
 db.define_table('participant_task', Field('participant', type='string'), Field('code', type='string'), Field('task_id', type='string'), Field('assist', type='boolean'))
